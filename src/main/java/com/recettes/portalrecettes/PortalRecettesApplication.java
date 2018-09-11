16412460980927dea04727d9480c3e408431f919
susko8 <samuel.susoliak@gmail.com>
2018-09-11 13:49:04 +0200

    Initial commit

---
+++ src/main/java/com/recettes/portalrecettes/PortalRecettesApplication.java
@@ -0,0 +1,13 @@
+package com.recettes.portalrecettes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalRecettesApplication
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalRecettesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/recettes/portalrecettes/PortalRecettesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.recettes.portalrecettes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PortalRecettesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
