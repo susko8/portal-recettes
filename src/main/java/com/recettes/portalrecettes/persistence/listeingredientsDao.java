22bdb1d4dd2083c11f8c0d9bf814aff109b9529e
So <sophie.aitis@epfedu.fr>
2018-09-17 16:05:56 +0200

    commit du 17/09 version_4

--- src/main/java/com/recettes/portalrecettes/persistence/listeingredientsDao.java
+++
@@ -1,9 +0,0 @@
-package com.recettes.portalrecettes.persistence;
-
-import com.recettes.portalrecettes.models.listeingredients;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface listeingredientsDao extends CrudRepository<listeingredients, Integer>{
-}
